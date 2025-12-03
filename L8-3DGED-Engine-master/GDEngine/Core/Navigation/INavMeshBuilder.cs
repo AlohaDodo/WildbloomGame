aa69034327d1ef41cdccecabaf9e733c2018bbff
AlohaDodo <d00265290@student.dkit.ie>
2025-12-03 17:45:44 +0000

    Added navigation

---
+++ L8-3DGED-Engine-master/GDEngine/Core/Navigation/INavMeshBuilder.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GDEngine.Core.Navigation
+{
+    /// <summary>
+    /// Builder abstraction for constructing navigation meshes.
+    /// </summary>
+    public interface INavMeshBuilder
+    {
+        #region Methods
+        void Build(INavGraphEditable navGraph);
+        #endregion
+    }
+}
